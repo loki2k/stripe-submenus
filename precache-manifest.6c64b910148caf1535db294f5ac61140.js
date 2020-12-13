2bae0948e0a20d3c74a22901cbc6b77e047b835d
loki2k <jeremylagarto@gmail.com>
2020-12-13 10:59:36 -0800

    Updates

---
+++ precache-manifest.6c64b910148caf1535db294f5ac61140.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "33089254e63fd6e8c3d899502479d172",
+    "url": "/stripe-submenus/index.html"
+  },
+  {
+    "revision": "94ad1af716bda5e63998",
+    "url": "/stripe-submenus/static/css/main.f8d94d5f.chunk.css"
+  },
+  {
+    "revision": "9d9ce4078b442a71cc60",
+    "url": "/stripe-submenus/static/js/2.e1e77988.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/stripe-submenus/static/js/2.e1e77988.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "94ad1af716bda5e63998",
+    "url": "/stripe-submenus/static/js/main.8945c63f.chunk.js"
+  },
+  {
+    "revision": "fb1405f2e1e392e6e32d",
+    "url": "/stripe-submenus/static/js/runtime-main.a65415c3.js"
+  },
+  {
+    "revision": "882a211ad0bae36b9c11715c9291f36b",
+    "url": "/stripe-submenus/static/media/hero.882a211a.svg"
+  },
+  {
+    "revision": "86d618b5be1508bcbbb31d6393f089f6",
+    "url": "/stripe-submenus/static/media/logo.86d618b5.svg"
+  },
+  {
+    "revision": "e908706c41907a9611ba9c4d61a32f7c",
+    "url": "/stripe-submenus/static/media/phone.e908706c.svg"
+  }
+]);
